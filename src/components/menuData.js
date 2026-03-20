f8d6518d4dcf4e9c7a458ab17d5b807a37e55b88
kik-work <kik.work100@gmail.com>
2026-03-21 02:13:11 +0600

    routing and layout updated with responsiveness

--- src/components/menuData.js
+++ src/components/menuData.js
@@ -1,59 +1,71 @@
 export const menuData = [
-    {
-        title: "About Us",
-        items: [
-            { name: "Our People", url: "/about/our-people" },
-            { name: "(We Will Provide)", url: "/about/we-will-provide" }
-        ],
-    },
-    {
-        title: "Destination",
-        items: [
-            { name: "Study In USA", url: "/destination/study-in-usa" },
-            { name: "Study In UK", url: "/destination/study-in-uk" },
-            { name: "Study In CANADA", url: "/destination/study-in-canada" },
-            { name: "Study In AUSTRALIA", url: "/destination/study-in-australia" },
-            { name: "Study In NZ", url: "/destination/study-in-nz" },
-            { name: "Study In IRELAND", url: "/destination/study-in-ireland" },
-            { name: "Study In EUROPE", url: "/destination/study-in-europe" },
-            { name: "Study In KOREA", url: "/destination/study-in-korea" },
-            { name: "Study In MALAYSIA", url: "/destination/study-in-malaysia" },
-            { name: "Study In CHINA", url: "/destination/study-in-china" }
-        ],
-    },
-    {
-        title: "Our Services",
-        items: [
-            { name: "Admission", url: "/services/admission" },
-            { name: "Scholarship", url: "/services/scholarship" },
-            { name: "Health Insurance", url: "/services/health-insurance" },
-            { name: "Visa Application", url: "/services/visa-application" },
-            { name: "Accommodation", url: "/services/accommodation" }
-        ],
-    },
-    {
-        title: "USA Mentorship Program (Bootcamp)",
-        items: [
-            { name: "Liberal Arts College", url: "/mentorship/liberal-arts-college" },
-            { name: "Elite School Admission", url: "/mentorship/elite-school-admission" },
-            { name: "Masters Mentorship Program", url: "/mentorship/masters-mentorship-program" },
-            { name: "Liberal Arts Education", url: "/mentorship/liberal-arts-education" },
-            { name: "Acceptance Letters", url: "/mentorship/acceptance-letters" },
-            { name: "Strategy Brainstorm", url: "/mentorship/strategy-brainstorm" },
-            { name: "Writing & Application", url: "/mentorship/writing-application" },
-            { name: "College Selection", url: "/mentorship/college-selection" },
-            { name: "Financial Aid", url: "/mentorship/financial-aid" },
-            { name: "Post Graduate Funding", url: "/mentorship/post-graduate-funding" }
-        ],
-    },
-    {
-        title: "Contact Us",
-        items: [
-            { name: "Our Address", url: "/contact/address" },
-            { name: "Email", url: "/contact/email" },
-            { name: "FB Page", url: "/contact/facebook" },
-            { name: "Instagram Id", url: "/contact/instagram" },
-            { name: "Contact No", url: "/contact/phone" }
-        ],
-    }
+  {
+    title: "About Us",
+    items: [
+      { name: "Our People", url: "/about/our-people" },
+      { name: "(We Will Provide)", url: "/about/we-will-provide" },
+    ],
+  },
+  {
+    title: "Destination",
+    items: [
+      { name: "Study In USA", url: "/destination/study-in-usa" },
+      { name: "Study In UK", url: "/destination/study-in-uk" },
+      { name: "Study In CANADA", url: "/destination/study-in-canada" },
+      { name: "Study In AUSTRALIA", url: "/destination/study-in-australia" },
+      { name: "Study In NZ", url: "/destination/study-in-nz" },
+      { name: "Study In IRELAND", url: "/destination/study-in-ireland" },
+      { name: "Study In EUROPE", url: "/destination/study-in-europe" },
+      { name: "Study In KOREA", url: "/destination/study-in-korea" },
+      { name: "Study In MALAYSIA", url: "/destination/study-in-malaysia" },
+      { name: "Study In CHINA", url: "/destination/study-in-china" },
+    ],
+  },
+  {
+    title: "Our Services",
+    items: [
+      { name: "Admission", url: "/services/admission" },
+      { name: "Scholarship", url: "/services/scholarship" },
+      { name: "Health Insurance", url: "/services/health-insurance" },
+      { name: "Visa Application", url: "/services/visa-application" },
+      { name: "Accommodation", url: "/services/accommodation" },
+    ],
+  },
+  {
+    title: "USA Mentorship Program (Bootcamp)",
+    items: [
+      { name: "Liberal Arts College", url: "/mentorship/liberal-arts-college" },
+      {
+        name: "Elite School Admission",
+        url: "/mentorship/elite-school-admission",
+      },
+      {
+        name: "Masters Mentorship Program",
+        url: "/mentorship/masters-mentorship-program",
+      },
+      {
+        name: "Liberal Arts Education",
+        url: "/mentorship/liberal-arts-education",
+      },
+      { name: "Acceptance Letters", url: "/mentorship/acceptance-letters" },
+      { name: "Strategy Brainstorm", url: "/mentorship/strategy-brainstorm" },
+      { name: "Writing & Application", url: "/mentorship/writing-application" },
+      { name: "College Selection", url: "/mentorship/college-selection" },
+      { name: "Financial Aid", url: "/mentorship/financial-aid" },
+      {
+        name: "Post Graduate Funding",
+        url: "/mentorship/post-graduate-funding",
+      },
+    ],
+  },
+  {
+    title: "Contact Us",
+    items: [
+      { name: "Our Address", url: "/contact/address" },
+      { name: "Email", url: "/contact/email" },
+      { name: "FB Page", url: "/contact/facebook" },
+      { name: "Instagram Id", url: "/contact/instagram" },
+      { name: "Contact No", url: "/contact/phone" },
+    ],
+  },
 ];
---
+++ src/pages/about/our-people.jsx
@@ -0,0 +1,11 @@
+const OurPeople = () => {
+  return (
+    <>
+      <div className="mx-auto m-4 px-4 md:px-8 lg:px-10 xl:px-10 2xl:flex 2xl:items-center max-w-7xl">
+        Hello World
+      </div>
+    </>
+  );
+};
+
+export default OurPeople;
